949d70e12989a38fa2f1ce59af77209a4eb8bf32
unknown <mohamedchebbidev@gmail.com>
2024-03-02 12:17:34 +0100

    fix: establich backend project architecture

--- fastify/config/seequilize.ts
+++ fastify/config/seequilize.ts
@@ -1,9 +1,14 @@
 import { Sequelize } from "sequelize";
 
 // Sequelize configuration
-const sequelize = new Sequelize("database", "username", "password", {
-  dialect: "postgres",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.POSTGRES_URL as string,
+  process.env.POSTGRES_URL as string,
+  process.env.POSTGRES_URL as string,
+  {
+    dialect: "postgres",
+    host: "localhost",
+  }
+);
 
 export { sequelize };
--- fastify/server.ts
+++ fastify/server.ts
@@ -3,11 +3,17 @@ import Register from "./controllers/UserController";
 
 // Define the User model
 
-const server = Fastify();
+const app = Fastify({
+  logger: true,
+});
 
-server.post("/register", Register);
+app.post("/register", Register);
 
-server.listen(5000, (err, address) => {
-  if (err) throw err;
-  console.log(`Server listening on ${address}`);
+const port = process.env.PORT || 5000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("SERVER  IS RUNNING ON PORT", port);
 });
